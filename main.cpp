5d1f275499f288b846f42e50849d14cb84329dc9
Mikhail <91342252+Mishachkld@users.noreply.github.com>
2022-11-12 11:29:59 +0200

    Работает

--- main.cpp
+++ main.cpp
@@ -6,105 +6,94 @@
 
 #define STR_SIZE 2600
 std::string FILE_PATH_IN = "input.txt";
-std::string FILE_PATH_OUT = "output.txt";
+std::string FILE_PATH_OUT = "TEXT.txt";
 
 char a, b;
 int N;
 
 void outArray(std::string* myString) {
-    std::ofstream file(FILE_PATH_OUT, std::ios::out);
-    for (int i = 0; i < N; i++) {
-        if (!myString[i].empty()) {
-            file << myString[i] << std::endl;
-        }
-    }
-    file.close();
+	std::ofstream file(FILE_PATH_OUT, std::ios::out);
+	for (int i = 0; i < N; i++) {
+		if (!myString[i].empty()) {
+			//std::cout << myString[i] << " ";
+			file << myString[i] << std::endl;
+		}
+	}
+	std::cout << std::endl;
+
+
+	file.close();
 }
 
 
-bool compareByLength(const std::string& firstStr, const std::string& lastStr) {
-    return firstStr.length() > lastStr.length();
+bool compareByLength(const std::string firstStr, const std::string lastStr) {
+	return firstStr.length() > lastStr.length();
 
 }
 
-std::string* mySort(std::string* myStrings) {
-    for (int i = 0; i < myStrings->length(); i++) {
-        for (int j = i + 1; j < myStrings->length(); j++) {
-            if (compareByLength(myStrings[i], myStrings[j]) and !myStrings[i].empty()
-                and !myStrings[j].empty()) {
-                std::swap(myStrings[i], myStrings[j]);
-            }
-        }
-    }
-    return myStrings;
+std::string* mySort(std::string* myStrings, int n) {
+	for (int i = 0; i < n-1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (compareByLength(myStrings[i], myStrings[j]) && !myStrings[i].empty()
+				&& !myStrings[j].empty()) {
+				std::swap(myStrings[i], myStrings[j]);
+			}
+		}
+	}
+	return myStrings;
 }
 
-int counts(std::string* s, std::string what) {
-    for (int i = 0; i < (s)->length(); ++i) {
-        if (s[i] == what)
-            return false;
-    }
-    return true;
+int counts(std::string* s, std::string what, int j) {
+	for (int i = 0; i < j; ++i) {
+		if (s[i] == what)
+			return false;
+	}
+	return true;
 }
 
 
 std::string remover(std::string str) {
-    std::string ret = "";
-    std::string exceptions = " ,;\n'"".?!:(){}[]*&^%$#@-_=+\\|/0123456789";
-    for (int i = 0; i <= str.length(); i++) {
-        if (exceptions.find(str[i]) == std::string::npos)
-            ret += str[i];
-    }
-    return ret;
+	std::string ret = "";
+	std::string exceptions = " ,;\n'"".?!:(){}[]*&^%$#@-_=+\\|/0123456789";
+	for (int i = 0; i < str.length(); i++) {
+		if (exceptions.find(str[i]) == std::string::npos)
+			ret += str[i];
+	}
+	return ret;
 }
 
 
-std::string makeStringLower(std::basic_string<char> word) {
-    for (int i = 0; i < word.length(); i++) {
-        if (isupper(word[i])) {
-            word[i] = tolower(word[i]);
-            std::cout << word[i] << " ";
-
-        }
-    }
-    return word;
-}
 
 int main() {
-    setlocale(0, "");
-    std::string word;
-    std::ifstream file(FILE_PATH_IN, std::ios::in);
-    file >> a;
-    file >> b;
-    file >> N;
-    std::cout << "In word's we have these liter: " << a << " " << b << std::endl;
-    std::string myString[STR_SIZE];
-    std::string resultString[STR_SIZE];
-    int j = 0;
-    while (!file.eof() and j < 2600) {
+	setlocale(0, "");
+	std::string word;
+	std::ifstream file(FILE_PATH_IN, std::ios::in);
+	file >> a;
+	file >> b;
+	file >> N;
+	std::cout << "In word's we have these liter: " << a << " " << b << std::endl;
+	std::string myString[STR_SIZE];
+	std::string resultString[STR_SIZE];
+	int j = 0;
+	while (!file.eof() && j < 2600) {
 
-        file >> word;
-        word = remover(word);
-        //word = makeStringLower(word);
-        std::transform(word.begin(), word.end(), word.begin(),
-            tolower);
-        if ((word.length() != 1) and (word.find(a) != std::string::npos) and (word.find(b) != std::string::npos)) {
-            myString[j] = word;
+		file >> word;
+		word = remover(word);
+		std::transform(word.begin(), word.end(), word.begin(),
+			tolower);
+		if ((word.length() != 1) && (word.find(a) != std::string::npos) && (word.find(b) != std::string::npos) && counts(myString, word, j)) {
+			myString[j] = word;
+			//std::cout << myString[j] << " ";
+			j++;
+		}
+	}
+	file.close();
+	//outArray(myString);
+	mySort(myString, j);
+	//outArray(myString);
 
-            j++;
-        }
-    }
-    file.close();
-    mySort(myString);
-    int counter = 0;
-    for (int i = 0; i < myString->length() and counter < N; i++) {
-        if (counts(resultString, myString[i])) {
-            resultString[i] = myString[i];
-            counter++;
-        }
-    }
+	outArray(myString);
+	system("pause");
 
-
-    outArray(resultString);
-    return 0;
+	return 0;
 }
